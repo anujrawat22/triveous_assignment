da1c9056de63aa40df4f86693fbbc6c33dd7e6a7
anujrawat22 <anuj22rawat20@gmail.com>
2023-07-26 18:57:25 +0530

    auth middlware added to cart and order routes

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,29 @@
-import exress from 'express'
-import dotenv from 'dotenv'
-import { connection } from './config/db'
-import userRouter from './routers/userRouter'
-import categoryRouter from './routers/categoryRouter'
-import productRouter from './routers/productRouter'
-import cartRouter from './routers/cartRouter'
-import orderRouter from './routers/orderRouter'
-dotenv.config()
-const app = exress()
-const port = process.env.PORT
+import exress from "express";
+import dotenv from "dotenv";
+import { connection } from "./config/db";
+import userRouter from "./routers/userRouter";
+import categoryRouter from "./routers/categoryRouter";
+import productRouter from "./routers/productRouter";
+import cartRouter from "./routers/cartRouter";
+import orderRouter from "./routers/orderRouter";
+import { authMiddlware } from "./middlewares/authenticationMiddleware";
+dotenv.config();
+const app = exress();
+const port = process.env.PORT;
 
-app.use(exress.json())
-app.use("/api/user",userRouter)
-app.use("/api/category",categoryRouter)
-app.use("/api/product",productRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use(exress.json());
+app.use("/api/user", userRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", authMiddlware, cartRouter);
+app.use("/api/order", authMiddlware, orderRouter);
 
-
-app.listen(port,async()=>{
-try {
-    await connection
-    console.log('Connected to DB');
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
     console.log(`Listening on PORT ${port}`);
-    
-    
-} catch (error) {
+  } catch (error) {
     console.log(error);
-    
-}    
-})
+  }
+});
