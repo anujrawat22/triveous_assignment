103b4ae39bf12778d5aadfe97f83f6963239d4fb
anujrawat22 <anuj22rawat20@gmail.com>
2023-07-26 14:43:29 +0530

    product model created

---
+++ src/models/productModel.ts
@@ -0,0 +1,61 @@
+import mongoose , { Schema ,Model , Document
+} from "mongoose";
+
+
+export interface IProduct extends Document {
+    title : string;
+    price : number;
+    description : string;
+    specification : string;
+    category_id : Schema.Types.ObjectId;
+    images : string[];
+    mainImage : string;
+    brand : string;
+    model : string;
+    colours : string[];
+    warrantyPeriod : number;
+}
+
+const productSchema : Schema<IProduct> = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    description :{
+        type : String,
+        required : true
+    },
+    specification : {
+        type : String,
+        required : true
+    },
+    category_id : {
+        type : Schema.Types.ObjectId,
+        ref : 'Category',
+        required : true
+    },
+    images : [{
+        type : String, required : true
+    }],
+    mainImage : {
+        type : String ,required : true
+    },
+brand : {
+    type : String,required : true
+},
+model : {
+    type : String,required : true
+},
+colours : [{
+    type : String,required : true
+}],
+warrantyPeriod : {
+    type : Number,required : true
+}
+})
+
+export const Product : Model<IProduct> = mongoose.model<IProduct>("Product",productSchema)
